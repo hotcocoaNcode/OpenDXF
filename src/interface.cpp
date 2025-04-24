2e03ccffd1289e40e543958971e6c259975ea689
cocoa <95043086+hotcocoaNcode@users.noreply.github.com>
2025-04-24 15:33:31 -0500

    grid lines

--- src/interface.cpp
+++ src/interface.cpp
@@ -32,7 +32,35 @@ namespace opendxf::interface {
     \
     void main()\
     {\
-        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\
+        FragColor = vec4(1.0f);\
+    }";
+
+    auto bg_vertex2d = "\
+    #version 330 core\
+    layout (location = 0) in vec2 pos;\
+    out vec2 uv;\
+    uniform vec4 world_transform;\
+    \
+    void main()\
+    {\
+        uv = (pos);\
+        gl_Position = vec4(pos.xy, 0.0, 1.0);\
+    }";
+
+    auto bg_fragment2d = "\
+    #version 330 core\
+    \
+    in vec2 uv;\
+    out vec4 out_color;\
+    uniform vec4 world_transform;\
+    \
+    const vec2 grid_space = vec2(0.5f);\
+    const float width = 0.05f;\
+    \
+    void main(){\
+        vec2 uv_in = mod((uv/world_transform.xy+world_transform.zw), grid_space);\
+        vec2 lines = smoothstep(vec2(1.0 - width), vec2(1.0), uv_in / grid_space);\
+        out_color = vec4(vec3(min(round(dot(lines.xy, vec2(1.0))), 1.0))*vec3(0.1), 1.0);\
     }";
 
     float quad_vertices[] = {
@@ -59,7 +87,7 @@ namespace opendxf::interface {
     object_ubo_locs[OBJECT_TYPE_MAX_VALUE],
     draw_modes[OBJECT_TYPE_MAX_VALUE];
 
-    unsigned int bg_vao, bg_vbo, bg_program;
+    unsigned int bg_vao, bg_vbo, bg_program, bg_global_ubo_loc;
 
     // Backend things
     unsigned int fbuf_width, fbuf_height;
@@ -68,7 +96,7 @@ namespace opendxf::interface {
     float view_x = 0.0f, view_y = 0.0f;
     double mouse_last_x = 0.0, mouse_last_y = 0.0;
     float sensitivity = 0.0025f;
-    float scale = 1.0;
+    float scale = 0.25f;
     bool plus_held = false, minus_held = false;
 
     void fbuf_callback(GLFWwindow* window, int width, int height)
@@ -135,6 +163,43 @@ namespace opendxf::interface {
         glfwGetFramebufferSize(window, &w, &h);
         fbuf_callback(window, w, h);
 
+        const unsigned int bg_vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(bg_vertex, 1, &bg_vertex2d, nullptr);
+        glCompileShader(bg_vertex);
+
+        const unsigned int bg_frag = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(bg_frag, 1, &bg_fragment2d, nullptr);
+        glCompileShader(bg_frag);
+        glGetError();
+        char info_log[512];
+        glGetShaderInfoLog(bg_frag, 512, nullptr, info_log);
+        std::cout << info_log << std::endl;
+
+        bg_program = glCreateProgram();
+        glAttachShader(bg_program, bg_vertex);
+        glAttachShader(bg_program, bg_frag);
+        glLinkProgram(bg_program);
+        glDeleteShader(bg_vertex);
+        glDeleteShader(bg_frag);
+
+        bg_global_ubo_loc = glGetUniformLocation(bg_program, "world_transform");
+
+        // Set up background grid resources
+        glGenVertexArrays(1, &bg_vao);
+        glGenBuffers(1, &bg_vbo);
+
+        glBindVertexArray(bg_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, bg_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // Rest of the resources
         global_vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(global_vertex_shader_id, 1, &shader_vertex2d, nullptr);
         glCompileShader(global_vertex_shader_id);
@@ -187,21 +252,32 @@ namespace opendxf::interface {
         if (scale < 0.0) scale = 0.25;
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        const float world_transform[4] = {(static_cast<float>(fbuf_height) / static_cast<float>(fbuf_width))*scale, (1.0f)*scale, view_x, view_y};
+
+        glUseProgram(bg_program);
+        glBindVertexArray(bg_vao);
+        glUniform4fv(static_cast<GLint>(bg_global_ubo_loc), 1, world_transform);
+        glBindBuffer(GL_ARRAY_BUFFER, bg_vbo);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+
         object_type last_bound_type = OBJECT_TYPE_MAX_VALUE;
         for (unsigned int i = 0; i < count; i++) {
             const auto& object = objects[i];
             const auto& type = object.type;
-            const float world_transform[4] = {(static_cast<float>(fbuf_height) / static_cast<float>(fbuf_width))*scale, (1.0f)*scale, view_x, view_y};
-            glUniform4fv(static_cast<GLint>(global_ubo_locs[type]), 1, world_transform);
+            if (last_bound_type != type) {
+                glUseProgram(programs[type]);
+
+                glBindVertexArray(vaos[type]);
+
+                glUniform4fv(static_cast<GLint>(global_ubo_locs[type]), 1, world_transform);
+
+                last_bound_type = type;
+            }
 
             glUniform2fv(static_cast<GLint>(object_ubo_locs[type]), 2, reinterpret_cast<const float*>(&objects[i].pos_a));
 
-            if (last_bound_type != type) {
-                glUseProgram(programs[type]);
-                glBindVertexArray(0);
-                glBindVertexArray(vaos[type]);
-                last_bound_type = type;
-            }
             glDrawArrays(draw_modes[type], 0, 2);
         }
 
